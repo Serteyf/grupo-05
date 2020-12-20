6973a267aef2d0d1df42da2561678a403f81a997
Damián Monti <damian@MacBook-Pro-de-Damian.local>
2020-12-20 09:59:33 -0300

    Le agregué a la data la propiedad outstanding, y puse los productos destacados del main

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,13 @@
+const getProducts = require("../utils/getProducts");
+const toThousand = require("../utils/toThousand");
+
 mainController = {
     home: (req, res) => {
-        res.render("index")
+        const products = getProducts();
+        res.render("index", {
+            products: products,
+            thousand: toThousand,
+        });
     }
 };
 

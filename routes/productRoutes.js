240d9c6b6d3817933b6c2faec005f74437cfa01e
SNovara <santinovara@hotmail.com>
2020-12-28 19:21:13 -0300

    Se corrigió ruta de multer

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 var storage = multer.diskStorage({ 
     destination: (req, file, cb) => {
-            cb(null, 'public/images/products')
+            cb(null, __dirname + '/../public/images/products')
     },
     filename: (req, file, cb) => {
             cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))

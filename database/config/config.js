5d9f6708037b5469f5c936cffd40c77b80af3c77
SNovara <santinovara@hotmail.com>
2021-01-15 23:07:10 -0300

    Se agregó Sequelize al proyecto

---
+++ database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null, //Coordinar nombre con el equipo. Tiene que ir entre comillas.//
+    "database": "database_development", //Coordinar nombre con el equipo.//
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}

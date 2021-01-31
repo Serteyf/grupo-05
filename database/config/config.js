e5c9b1263c0ac3bc995d97e0354b24e4bc35225b
SNovara <santinovara@hotmail.com>
2021-01-31 12:02:00 -0300

    Actualización de archivo 'config.js'

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null, //Coordinar nombre con el equipo. Tiene que ir entre comillas.//
-    "database": "database_development", //Coordinar nombre con el equipo.//
+    "database": "level_up", //Esto se cambió, pero me tira un mensaje raro cuando hago "git add ." El mensaje es "warning: LF will be replaced by CRLF in database/config/config.js. The file will have its original line endings in your working directory". //
     "host": "127.0.0.1",
     "dialect": "mysql"
   },

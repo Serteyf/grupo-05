f0fde537785c306e7efdb70b4412df9052b997a3
marcosdellavecchia <marcosdellavecchia@gmail.com>
2021-02-05 19:14:04 -0300

    Se corrigio estructura de base de datos

--- database/migrations/structure.sql
+++ database/migrations/structure.sql
@@ -1,11 +1,15 @@
+DROP DATABASE IF EXISTS level_up;
 CREATE DATABASE level_up;
+USE level_up;
 
+DROP TABLE IF EXISTS users_category;
 CREATE TABLE users_category (
 id INTEGER AUTO_INCREMENT,
 name VARCHAR(50),
 PRIMARY KEY (id)
 ); 
 
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 id INTEGER AUTO_INCREMENT,
 user VARCHAR(50),
@@ -13,18 +17,20 @@ name VARCHAR(50),
 email VARCHAR(50),
 address VARCHAR(50),
 password VARCHAR(50),
-category_id INTEGER,
+categoryId INTEGER,
 avatar VARCHAR(50),
 PRIMARY KEY (id),
-FOREIGN KEY (category_id) REFERENCES users_category(id)
+FOREIGN KEY (categoryId) REFERENCES users_category(id)
 ); 
 
+DROP TABLE IF EXISTS products_category;
 CREATE TABLE products_category (
 id INTEGER AUTO_INCREMENT,
 name VARCHAR(50),
 PRIMARY KEY (id)
 ); 
 
+DROP TABLE IF EXISTS products;
 CREATE TABLE products (
 id INTEGER AUTO_INCREMENT,
 name VARCHAR(50),
@@ -33,28 +39,30 @@ price INTEGER,
 discount INTEGER,
 featured INTEGER,
 image VARCHAR(50),
-category_id INTEGER,
+categoryId INTEGER,
 PRIMARY KEY (id),
-FOREIGN KEY (category_id) REFERENCES products_category(id)
+FOREIGN KEY (categoryId) REFERENCES products_category(id)
 ); 
 
+DROP TABLE IF EXISTS sales;
 CREATE TABLE sales (
 id INTEGER AUTO_INCREMENT,
 date DATE,
-payment_method VARCHAR(50),
-quantity INTEGER,
-total_amount INTEGER,
-user_id INTEGER,
+paymentMethod VARCHAR(50),
+userId INTEGER,
 PRIMARY KEY (id),
-FOREIGN KEY (user_id) REFERENCES users(id)
+FOREIGN KEY (userId) REFERENCES users(id)
 ); 
 
+DROP TABLE IF EXISTS products_sales;
 CREATE TABLE products_sales (
 id INTEGER AUTO_INCREMENT,
-product_id INTEGER,
-sale_id INTEGER,
+productId INTEGER,
+saleId INTEGER,
+quantity INTEGER,
+amount INTEGER,
 PRIMARY KEY (id),
-FOREIGN KEY (product_id) REFERENCES products(id),
-FOREIGN KEY (sale_id) REFERENCES sales(id)
+FOREIGN KEY (productId) REFERENCES products(id),
+FOREIGN KEY (saleId) REFERENCES sales(id)
 ); 
 

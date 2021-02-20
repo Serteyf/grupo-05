80a1461f2537cb31a8b5241707194cac01b459e7
marcosdellavecchia <marcosdellavecchia@gmail.com>
2021-02-20 20:40:51 -0300

    Se implemento base de datos en busqueda de productos

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -19,24 +19,31 @@ mainController = {
             });
     },
     search: (req, res) => {
-        const products = getProducts();
-        let keyword = req.query.buscar.toLowerCase();
-        let keywordReplace = replaceAll(keyword);
-        let results = [];
+        db.Product.findAll({
+            raw: true,
+        })
+            .then((products) => {
+                let keyword = req.query.buscar.toLowerCase();
+                let keywordReplace = replaceAll(keyword);
+                let results = [];
 
-        for (let i = 0; i < products.length; i++) {
-            const product = products[i].name.toLowerCase();
-            const productReplace = replaceAll(product);
-            if (productReplace.includes(keywordReplace)) {
-                results.push(products[i]);
-            }
-        }
+                for (let i = 0; i < products.length; i++) {
+                    const product = products[i].name.toLowerCase();
+                    const productReplace = replaceAll(product);
+                    if (productReplace.includes(keywordReplace)) {
+                        results.push(products[i]);
+                    }
+                }
 
-        res.render("product-search", {
-            results: results,
-            keyword: keyword,
-            thousand: toThousand,
-        });
+                res.render("product-search", {
+                    results: results,
+                    keyword: keyword,
+                    thousand: toThousand,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     },
     contact: (req, res) => {
         res.render("contact");

40fec930911ce4dc4ba67707ae2ce02c32fa6b1f
marcosdellavecchia <marcosdvecchia@gmail.com>
2021-03-04 20:27:52 -0300

    Agregada estructura de carpetas de componentes

---
+++ admin-dashboard/src/components/SideMenu/SideMenu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SideMenu = () => {
+  return (
+    <div>
+      <p>Side Menu</p>
+    </div>
+  );
+};
+
+export default SideMenu;
---
+++ admin-dashboard/src/components/SideMenu/SideMenuLink/SideMenuLink.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SideMenuLink = () => {
+  return (
+    <div>
+      <p>Side Menu Link</p>
+    </div>
+  );
+};
+
+export default SideMenuLink;

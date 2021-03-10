cc2748950148794b48b9a6f8e4fd0e18a2c706f4
marcosdellavecchia <marcosdvecchia@gmail.com>
2021-03-10 15:20:47 -0300

    Corregida validacion de imagen en register

--- site/public/js/validations/userValidations.js
+++ site/public/js/validations/userValidations.js
@@ -3,11 +3,9 @@ let errors = [];
 window.addEventListener("load", () => {
     const form = document.getElementById("registerForm");
 
-    
-    
     form.addEventListener("submit", (event) => {
         // event.preventDefault();
-        
+
         errors = [];
         clearValidations();
 
@@ -42,48 +40,37 @@ window.addEventListener("load", () => {
                 "Password es un campo requerido y con mas de 7 caracteres!",
             ],
         ]);
-        const avatarInput = document.getElementById("avatar")
-        
+        const avatarInput = document.getElementById("avatar");
+
         validateFile(avatarInput);
-        
+
         const passwordInput = document.getElementById("password");
         const password2Input = document.getElementById("password2");
-    
-    
-        
-            validateInput("password", [
-                [
-                    validator.isLength,
-                    { min: 8 },
-                    "Tu contraseña tiene que tener al menos 8 caracteres!",
-                ],
-                // [validator.isLength, { min: 1 }, "Tiene que tener al menos una letra minuscula!"],
-                // [validator.isLength, { min: 1 }, "Tiene que tener al menos una letra mayuscula!"],
-                // [validator.isLength, { min: 1 }, "Tiene que tener al menos un simbolo!"],
-                // [validator.isLength, { min: 1 }, "Tiene que tener al menos un numero!"],
-            ]);
-            comparePassword (passwordInput, password2Input);
-            checkErrors();
-            
-            if (checkErrors()) {
-                event.preventDefault();
-            }
-  
-        
-        });
-    
 
-    
+        validateInput("password", [
+            [
+                validator.isLength,
+                { min: 8 },
+                "Tu contraseña tiene que tener al menos 8 caracteres!",
+            ],
+            // [validator.isLength, { min: 1 }, "Tiene que tener al menos una letra minuscula!"],
+            // [validator.isLength, { min: 1 }, "Tiene que tener al menos una letra mayuscula!"],
+            // [validator.isLength, { min: 1 }, "Tiene que tener al menos un simbolo!"],
+            // [validator.isLength, { min: 1 }, "Tiene que tener al menos un numero!"],
+        ]);
+        comparePassword(passwordInput, password2Input);
+        checkErrors();
 
-
-
-    
+        if (checkErrors()) {
+            event.preventDefault();
+        }
+    });
 });
 
 function clearValidations() {
     const arrayInputs = document.getElementsByClassName("validate");
     const arrayFeedbacks = document.getElementsByClassName("feedback");
-    const arrayBorder = document.getElementsByClassName("feedback-border")
+    const arrayBorder = document.getElementsByClassName("feedback-border");
 
     for (const input of arrayInputs) {
         input.classList.remove("is-invalid", "is-valid");
@@ -92,8 +79,8 @@ function clearValidations() {
         feedback.classList.remove("alert", "alert-danger");
         feedback.innerHTML = "";
     }
-    for (const border of arrayBorder){
-        border.classList.remove("error-feedback-border")
+    for (const border of arrayBorder) {
+        border.classList.remove("error-feedback-border");
     }
 }
 
@@ -141,7 +128,7 @@ function checkErrors() {
             // const floatingInput = document.getElementsByClassName("floating-input");
             // floatingInput.classlist.add("invalid-input")
             input.classList.add("is-invalid");
-            borderDiv.classList.add("error-feedback-border")
+            borderDiv.classList.add("error-feedback-border");
             feedbackDiv.innerHTML = e.msg;
             feedbackDiv.classList.add("alert-danger");
         });
@@ -155,61 +142,51 @@ function isTrue(value) {
     return value;
 }
 
-function comparePassword (p1, p2) {
+function comparePassword(p1, p2) {
     let foundErrors = false;
 
     if (p1.value !== p2.value) {
         const error = {
             inputId: "password2",
-            msg: "Las contraseñas deben ser iguales"
+            msg: "Las contraseñas deben ser iguales",
         };
         errors.push(error);
         foundErrors = true;
-    } 
+    }
     if (!foundErrors) {
         p2.classList.add("is-valid");
     }
 }
 
+function validateFile(avatar) {
+    const allowedExtensions = ["jpg", "png", "jpeg", "gif"],
+        sizeLimit = 3000000; // 3 megabyte
 
-function validateFile(avatar){
-  const allowedExtensions =  ['jpg','png', 'jpeg', 'gif'],
-        sizeLimit = 3000000; // 1 megabyte
+    const error = {
+        inputId: "avatar",
+        msg: "",
+    };
 
-        const error = {
-            inputId: "avatar",
-            msg: ""
-        };
-        
-        if (!avatar.value){
-            error.msg = "Debe subir una imagen";
-            errors.push(error);
-            checkErrors();
-            return
-          } 
-  // destructuring file name and size from file object
-  const { name:fileName, size:fileSize } = avatar.files[0];
+    if (!avatar.value) {
+        error.msg = "Debe subir una imagen";
+        errors.push(error);
+        checkErrors();
+        return;
+    }
+    const { name: fileName, size: fileSize } = avatar.files[0];
 
-  /*
-  * if filename is apple.png, we split the string to get ["apple","png"]
-  * then apply the pop() method to return the file extension
-  *
-  */
-  const fileExtension = fileName.split(".").pop();
+    const fileExtension = fileName.split(".").pop();
 
-  /* 
-    check if the extension of the uploaded file is included 
-    in our array of allowed file extensions
-  */
-  if(!allowedExtensions.includes(fileExtension)){
-    error.msg = "La imagen debe ser en formato jpg, jpeg, png ó gif";
-    avatar.value = null;
-  }else if(fileSize > sizeLimit){
-    error.msg = "La imagen debe ser de 3 mb como máximo";
-    avatar.value = null;
-  } 
-  errors.push(error);
-  checkErrors();
+    if (!allowedExtensions.includes(fileExtension)) {
+        error.msg = "La imagen debe ser en formato jpg, jpeg, png o gif";
+        avatar.value = null;
+        errors.push(error);
+        checkErrors();
+    } else if (fileSize > sizeLimit) {
+        error.msg = "La imagen debe ser de 3 mb como máximo";
+        avatar.value = null;
 
+        errors.push(error);
+        checkErrors();
+    }
 }
-
